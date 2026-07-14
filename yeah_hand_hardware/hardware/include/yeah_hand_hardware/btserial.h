89e5af5d9fb2737c11c5294fda365a40b5137f97
Vittorio Lumare <vittorio.lumare@robotgarage.org>
2026-07-14 15:19:25 +0200

    added bt serial class
    Signed-off-by: Vittorio Lumare <vittorio.lumare@robotgarage.org>

---
+++ yeah_hand_hardware/hardware/include/yeah_hand_hardware/btserial.h
@@ -0,0 +1,82 @@
+#pragma once
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+#include <string>
+#include <stdexcept>
+#include <iostream>
+
+class BtSerial {
+public:
+  BtSerial(const std::string& dev = "/dev/rfcomm0", speed_t baud = B115200)
+  : fd_(-1)
+  {
+    fd_ = open(dev.c_str(), O_RDWR | O_NOCTTY | O_SYNC);
+    if (fd_ < 0) {
+      throw std::runtime_error("open failed: " + std::string(strerror(errno)));
+    }
+
+    struct termios tty{};
+    if (tcgetattr(fd_, &tty) != 0) {
+      close(fd_);
+      throw std::runtime_error("tcgetattr failed: " + std::string(strerror(errno)));
+    }
+
+    cfsetospeed(&tty, baud);
+    cfsetispeed(&tty, baud);
+
+    tty.c_cflag = (tty.c_cflag & ~CSIZE) | CS8;
+    tty.c_iflag &= ~IGNBRK;
+    tty.c_lflag = 0;
+    tty.c_oflag = 0;
+    tty.c_cc[VMIN]  = 0;
+    tty.c_cc[VTIME] = 10;   // 1.0 s read timeout
+
+    tty.c_iflag &= ~(IXON | IXOFF | IXANY);
+    tty.c_cflag |= (CLOCAL | CREAD);
+    tty.c_cflag &= ~(PARENB | PARODD);
+    tty.c_cflag &= ~CSTOPB;
+    tty.c_cflag &= ~CRTSCTS;
+
+    if (tcsetattr(fd_, TCSANOW, &tty) != 0) {
+      close(fd_);
+      throw std::runtime_error("tcsetattr failed: " + std::string(strerror(errno)));
+    }
+  }
+
+  ~BtSerial() {
+    if (fd_ >= 0) {
+      close(fd_);
+    }
+  }
+
+  bool writeString(const std::string& msg) {
+    const char* data = msg.c_str();
+    size_t total = 0;
+    size_t len = msg.size();
+
+    while (total < len) {
+      ssize_t n = write(fd_, data + total, len - total);
+      if (n < 0) {
+        if (errno == EINTR) continue;
+        std::cerr << "write failed: " << strerror(errno) << std::endl;
+        return false;
+      }
+      total += static_cast<size_t>(n);
+    }
+
+    tcdrain(fd_);
+    return true;
+  }
+
+  bool isOpen(){
+    return fd_ >= 0;
+  }
+
+private:
+  int fd_;
+};
+
